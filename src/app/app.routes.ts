f5843db22cd874a4197e1b7e3914e25847e3c88c
Eider Prieto <prietoeider1@gmail.com>
2025-03-19 13:05:08 -0500

    error correction with links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
+import { TermsServicesComponent } from './components/terms-services/terms-services.component';
 
 export const routes: Routes = [
     {   path: '', component: HomeComponent },
     {   path: 'about', component: AboutComponent },
     {   path: 'contact', component: ContactComponent },
     {   path: 'privacy&policy', component: PrivacyPolicyComponent },
+    {   path: 'termsAndConditions', component: TermsServicesComponent },
     {   path: '**', redirectTo: '' }
 ];

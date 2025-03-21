bdb6bff5570414c6ec10c5ef49b7a36530b602ec
Eider Prieto <prietoeider1@gmail.com>
2025-03-21 08:07:55 -0500

    adding tap

--- src/app/components/reserve/reserve.component.ts
+++ src/app/components/reserve/reserve.component.ts
@@ -1,9 +1,14 @@
 import { Component} from '@angular/core';
 import { PhotoService } from '../../services/photo.service';
-
+import { StepperModule } from 'primeng/stepper';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-reserve',
+  imports: [
+    StepperModule,
+    ButtonModule
+  ],
   templateUrl: './reserve.component.html',
   styleUrl: './reserve.component.css'
 })

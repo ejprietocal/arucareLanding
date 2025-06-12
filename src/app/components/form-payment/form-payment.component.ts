37c01a40f4bd2a7eb6b1f51acd0f16beb6acf1d3
Eider Prieto <prietoeider1@gmail.com>
2025-06-12 09:26:41 -0500

    required

--- src/app/components/form-payment/form-payment.component.ts
+++ src/app/components/form-payment/form-payment.component.ts
@@ -31,7 +31,7 @@ export class FormPaymentComponent {
   loaderService = inject(LoaderService);
 
   form = this.fb.group({
-    ccnumber: ['', [Validators.required, Validators.pattern('^[0-9]{16}$')]],
+    ccnumber: ['', [Validators.required,Validators.pattern('^([0-9]{4} ){3}[0-9]{4}$') ]],
     ccexp : ['', [Validators.required, Validators.pattern('^[0-9]{2}/[0-9]{2}$')]],
     cvv : ['', Validators.required],
     amount : ['', [Validators.required, Validators.pattern(/^[0-9]+(\.[0-9]+)?$/)] ],
@@ -54,10 +54,11 @@ export class FormPaymentComponent {
       this.loaderService.hideLoader();
       return;
     }
-
+    const rawValue = this.form.getRawValue();
+    rawValue.ccnumber = rawValue.ccnumber?.replace(/\s+/g, '') || '';
 
     if(this.form.get('ccnumber')!.value == '4111111111111111'){
-      this.paymentSuscription = this.formPaymentService.submitPaymentTest(this.form.value).subscribe({
+      this.paymentSuscription = this.formPaymentService.submitPaymentTest(rawValue).subscribe({
         next: (data) => {
 
           const result: Record<string, string> = {};
@@ -80,7 +81,6 @@ export class FormPaymentComponent {
           } else {
             this.toastr.error(result['responsetext'] || 'Error en la transacción');
           }
-
           this.loaderService.hideLoader();
 
         },
@@ -91,7 +91,8 @@ export class FormPaymentComponent {
       });
     }
     else{
-      this.paymentSuscription = this.formPaymentService.submitPayment(this.form.value).subscribe({
+      console.log(rawValue, this.form.value);
+      this.paymentSuscription = this.formPaymentService.submitPayment(rawValue).subscribe({
         next: (data) => {
           const result: Record<string, string> = {};
           data.response.split('&').forEach((pair : any) => {
@@ -137,6 +138,18 @@ export class FormPaymentComponent {
       if (value !== formatted) {
         this.form.get('ccexp')?.setValue(formatted, { emitEvent: false });
       }
+    }
+  });
+
+  this.changesSuscription =this.form.get('ccnumber')?.valueChanges.subscribe((value) => {
+    if (!value) return;
+
+    const cleaned = value.replace(/\D+/g, '').slice(0, 16); // Solo números, máx 16
+    const groups = cleaned.match(/.{1,4}/g); // Agrupa de a 4
+    const formatted = groups ? groups.join(' ') : '';
+
+    if (value !== formatted) {
+      this.form.get('ccnumber')?.setValue(formatted, { emitEvent: false });
     }
   });
 }
